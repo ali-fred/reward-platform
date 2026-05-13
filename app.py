12a1552a40a115c477380636431b42a9664aab87
Lumière Du Progrès <ndayishimiyealfred64@gmail.com>
2026-05-13 18:07:37 +0200

    render fix

--- app.py
+++ app.py
@@ -1159,8 +1159,4 @@ def logout():
 # RUN APP
 # =========================
 if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
+app.run(host='0.0.0.0', port=5000)
